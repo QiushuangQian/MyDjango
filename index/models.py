056ce41f622ceb540d4d43f2db4966c62186a27c
qqs <qiushuangqian@sina.com>
2020-01-28 12:32:04 +0800

    在网页中展示数据

--- index/models.py
+++ index/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+
 # Create your models here.
+# 在这搭建与数据库的映射
+
+# 在网页中展示数据：
+# 定义数据模型，以类的方式定义数据表的字段。在数据库创建数据表时，数据表由模型定义的类生成
+# 在视图导入模型所定义的类，该类称为数据表对象，Django为数据表对象提供独有的数据操作方法，可以实现数据库操作，获取数据
+# 视图函数获取数据后，将数据以字典、列表、对象的方式传递给HTML模板，由模板引擎接收和解析，生成相应网页
+
+# Product类与Product表构成映射关系，代码只搭建关系
+# python manage.py xxx 指令创建数据表
+class Product(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=50)
+    type = models.CharField(max_length=20)
--- index/views.py
+++ index/views.py
@@ -2,11 +2,26 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 import csv
 
-
 # Create your views here.
+from index.models import Product
+
+
 def index(request):
     # return HttpResponse("Hello World.")
-    return render(request,'index.html',context={'title':'首页'},status=500)
+    # render(request, template_name, context=None, content_type=None, status=None, using=None)
+    # request：请求对象
+    # template_name：HTML模板文件名
+    # context：对HTML模板的变量赋值（字典形式）
+    # content_type：响应数据的数据格式
+    # status：HTTP状态码，默认为200
+    # using：模板—>网页的模板引擎
+
+    # type_list用于查询数据表字段type的数据并将数据去重
+    type_list = Product.objects.values('type').distinct()
+    # name_list用于查询数据表字段type和name的全部数据
+    name_list = Product.objects.values('name', 'type')
+    context = {'title': '首页', 'type_list': type_list, 'name_list': name_list}
+    return render(request, 'index.html', context=context, status=200)
 
 
 def mydate(request, year, month, day):
@@ -23,7 +38,7 @@ def myyear_dict(request, year, month):
     return render(request, 'myyear_dict.html', {'month': month})
 
 
-#文件下载
+# 文件下载
 def download(request):
     # 当接收到用户请求后，视图函数download首先定义HttpResponse的响应类型为文件（text/csv）类型，生成response对象
     response = HttpResponse(content_type='text/csv')
@@ -32,13 +47,14 @@ def download(request):
     # attachment设置文件下载的方式，filename为文件名
     response['Content-Disposition'] = 'attachment;filename ="somefilename.csv"'
 
-    #使用CSV模块加载response对象，把数据写入response对象所设置的CSV文件并将response对象返回到浏览器上
+    # 使用CSV模块加载response对象，把数据写入response对象所设置的CSV文件并将response对象返回到浏览器上
     writer = csv.writer(response)
     writer.writerow(['First row', 'A', 'B', 'C'])
     return response
 
+
 def login(request):
-    #相对路径
+    # 相对路径
     # return redirect('/')
-    #绝对路径
+    # 绝对路径
     return redirect('http://127.0.0.1:8000/')
